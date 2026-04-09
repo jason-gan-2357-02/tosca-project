62dc48fae62aec25e1037312e2fb76902090fb86
Jason Gan <jason.gan@pointwest.com>
2026-04-09 13:53:07 +0800

    Initial commit

---
+++ create-databases-and-backup-job.sql
@@ -0,0 +1,234 @@
+USE [master];
+GO
+
+-- Create tosca_common repo database
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'tosca_common')
+BEGIN
+    CREATE DATABASE [tosca_common];
+    PRINT 'Database [tosca_common] created.';
+END
+ELSE
+BEGIN
+    PRINT 'Database [tosca_common] already exists.';
+END
+GO
+
+-- Create tosca_services database
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'tosca_services')
+BEGIN
+    CREATE DATABASE [tosca_services];
+	ALTER DATABASE [tosca_services] SET READ_COMMITTED_SNAPSHOT ON;
+    PRINT 'Database [tosca_services] created.';
+END
+ELSE
+BEGIN
+    PRINT 'Database [tosca_services] already exists.';
+END
+GO
+
+-- Create tools database
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'tools')
+BEGIN
+    CREATE DATABASE [tools];
+    PRINT 'Database [tools] created.';
+END
+ELSE
+BEGIN
+    PRINT 'Database [tools] already exists.';
+END
+GO
+
+-- Create server login
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'tosca_user')
+BEGIN
+    CREATE LOGIN [tosca_user] 
+    WITH PASSWORD = 'ToscaUserPassword123$', 
+    DEFAULT_DATABASE = [tosca_common],
+    CHECK_EXPIRATION = OFF,
+    CHECK_POLICY = ON;
+    PRINT 'Login [tosca_user] created.';
+END
+ELSE
+BEGIN
+    PRINT 'Login [tosca_user] already exists.';
+END
+GO
+
+-- Grant [tosca_user] full access to [tosca_common]
+USE [tosca_common];
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'tosca_user')
+BEGIN
+    CREATE USER [tosca_user] FOR LOGIN [tosca_user];
+    
+    -- Add user to the db_owner role for full access
+    ALTER ROLE [db_owner] ADD MEMBER [tosca_user];
+    
+    PRINT 'User [tosca_user] created in [tosca_common] and granted db_owner permissions.';
+END
+ELSE
+BEGIN
+    PRINT 'User [tosca_user] already exists in [tosca_common].';
+END
+GO
+
+-- Grant [tosca_user] full access to [tosca_services]
+USE [tosca_services];
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'tosca_user')
+BEGIN
+    CREATE USER [tosca_user] FOR LOGIN [tosca_user];
+    
+    -- Add user to the db_owner role for full access
+    ALTER ROLE [db_owner] ADD MEMBER [tosca_user];
+    
+    PRINT 'User [tosca_user] created in [tosca_services] and granted db_owner permissions.';
+END
+ELSE
+BEGIN
+    PRINT 'User [tosca_user] already exists in [tosca_services].';
+END
+GO
+
+-- Create backup_all_databases stored procedure in [tools]
+USE [tools];
+GO
+
+CREATE OR ALTER PROCEDURE [backup_all_databases]
+AS
+BEGIN
+
+	DECLARE @backuproot VARCHAR(40);
+	DECLARE @backupdir  VARCHAR(40);
+	DECLARE @dbname     VARCHAR(40);
+	DECLARE @filepath   VARCHAR(100);
+	DECLARE @rightnow   DATETIME = GETDATE();
+
+	SET @backuproot = 'C:\Backup';
+
+	IF DATEPART(HOUR, @rightnow) = 0
+	BEGIN
+		SET @backupdir = @backuproot + '\Daily\' + FORMAT(@rightnow, 'yyyy-MMdd-HHmmss');
+	END
+	ELSE
+	BEGIN
+		SET @backupdir = @backuproot + '\Hourly\' + FORMAT(@rightnow, 'yyyy-MMdd-HHmmss');
+	END
+
+	EXEC master.sys.xp_create_subdir @backupdir;
+
+	SET @dbname = 'master';
+	SET @filepath = @backupdir + '\' + @dbname + '.bak';
+	BACKUP DATABASE @dbname TO DISK = @filepath WITH INIT, COMPRESSION, STATS = 10;
+
+	SET @dbname = 'msdb';
+	SET @filepath = @backupdir + '\' + @dbname + '.bak';
+	BACKUP DATABASE @dbname TO DISK = @filepath WITH INIT, COMPRESSION, STATS = 10;
+
+	SET @dbname = 'model';
+	SET @filepath = @backupdir + '\' + @dbname + '.bak';
+	BACKUP DATABASE @dbname TO DISK = @filepath WITH INIT, COMPRESSION, STATS = 10;
+
+	SET @dbname = 'tools';
+	SET @filepath = @backupdir + '\' + @dbname + '.bak';
+	BACKUP DATABASE @dbname TO DISK = @filepath WITH INIT, COMPRESSION, STATS = 10;
+
+	SET @dbname = 'tosca_common';
+	SET @filepath = @backupdir + '\' + @dbname + '.bak';
+	BACKUP DATABASE @dbname TO DISK = @filepath WITH INIT, COMPRESSION, STATS = 10;
+
+	SET @dbname = 'tosca_services';
+	SET @filepath = @backupdir + '\' + @dbname + '.bak';
+	BACKUP DATABASE @dbname TO DISK = @filepath WITH INIT, COMPRESSION, STATS = 10;
+
+END;
+GO
+
+PRINT 'Stored procedure [backup_all_databases] created.';
+
+-- Create job for automated backup of all databases
+USE [msdb];
+GO
+
+DECLARE @jobId BINARY(16);
+DECLARE @jobName NVARCHAR(50);
+DECLARE @schedId INT;
+DECLARE @schedName NVARCHAR(50);
+
+SET @jobName = N'Automated_Backups';
+SET @schedName = N'Schedule_Every_4_Hours';
+
+-- Check if schedule already exists and delete it
+IF EXISTS (SELECT schedule_id FROM dbo.sysschedules WHERE name = @schedName)
+BEGIN
+    PRINT 'Schedule [' + @schedName + '] already exists. Deleting...';
+END	
+
+DECLARE sched_cursor CURSOR FOR 
+    SELECT schedule_id 
+    FROM dbo.sysschedules 
+    WHERE name = @schedName;
+
+OPEN sched_cursor;
+FETCH NEXT FROM sched_cursor INTO @schedId;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    EXEC dbo.sp_delete_schedule 
+        @schedule_id = @schedId, 
+        @force_delete = 1; 
+    FETCH NEXT FROM sched_cursor INTO @schedId;
+END
+
+CLOSE sched_cursor;
+DEALLOCATE sched_cursor;
+
+-- Check if the job already exists and delete it
+IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = @jobName)
+BEGIN
+    PRINT 'Job [' + @jobName + '] already exists. Deleting...';
+    EXEC sp_delete_job @job_name = @jobName, @delete_unused_schedule = 1;
+END
+
+-- 1. Create the Job
+EXEC sp_add_job 
+    @job_name = @jobName, 
+    @enabled = 1, 
+    @description = N'Executes the backup_databases procedure every 4 hours.',
+    @job_id = @jobId OUTPUT;
+
+-- 2. Add the Step
+EXEC sp_add_jobstep 
+    @job_id = @jobId, 
+    @step_name = N'Run Backup Stored Procedure', 
+    @subsystem = N'TSQL', 
+    @command = N'EXEC dbo.backup_all_databases;', 
+    @database_name = N'tools',
+    @retry_attempts = 1,
+    @retry_interval = 5;
+
+-- 3. Create the Schedule (Every day, every 4 hours)
+-- freq_type 4 = Daily
+-- freq_subday_type 8 = Hours
+-- freq_subday_interval 4 = Every 4 hours
+EXEC sp_add_schedule 
+    @schedule_name = @schedName, 
+    @freq_type = 4, 
+    @freq_interval = 1, 
+    @freq_subday_type = 8, 
+    @freq_subday_interval = 4, 
+    @active_start_time = 000000; -- Start at midnight (HHMMSS)
+
+-- 4. Attach the Schedule to the Job
+EXEC sp_attach_schedule 
+    @job_id = @jobId, 
+    @schedule_name = @schedName;
+
+-- 5. Target the Job to the local server
+EXEC sp_add_jobserver 
+    @job_id = @jobId, 
+    @server_name = N'(local)';
+
+PRINT 'Job [' + @jobName + '] with schedule [' + @schedName + '] created.';
