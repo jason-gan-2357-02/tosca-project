8abc532380300822880d5e09eb7250dc7873d199
ptc-rlim <robert.lim@pointwest.com>
2026-05-06 13:29:54 +0800

    Add fileService db handling

--- tosca-server-scripts/restore-attachments.ps1
+++ tosca-server-scripts/restore-attachments.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true, HelpMessage = "Please provide backup file.")]
+    [Parameter(Mandatory = $true, HelpMessage = "Please provide backup file yyyy-mm-dd-hhmmss_FILE.zip.")]
     [ValidateNotNullOrEmpty()]
     [ValidateScript({
         if (Test-Path $_ -PathType Leaf) {
@@ -9,11 +9,24 @@ param (
             throw "The backup file '$_' does not exist or is not a file."
         }
     })]
-    [string]$BackupFile
+    [string]$BackupFile,
+	
+    [Parameter(Mandatory = $true, HelpMessage = "Please provide backup file yyyy-mm-dd-hhmmss _FS.zip.")]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({
+        if (Test-Path $_ -PathType Leaf) {
+            $true
+        } else {
+            throw "The backup file '$_' does not exist or is not a file."
+        }
+    })]
+    [string]$BackupFileFS
 )
 
 $TargetDirectory = "G:\Tosca_Storage"
-$StagingDirectory = "G:\Restore_Staging"
+$StagingDirectory = "G:\Restore_Staging\File"
+$TargetFSDirectory = "G:\ProgramData\TRICENTIS\ToscaServer\FileService"
+$StagingFSDirectory = "G:\Restore_Staging\FS"  
 
 if (Test-Path $TargetDirectory -PathType Container) {
     $true
@@ -26,3 +39,15 @@ Remove-Item -Path $StagingDirectory\* -Recurse -Force
 Expand-Archive -Path $BackupFile -DestinationPath $StagingDirectory -Force
 robocopy $StagingDirectory $TargetDirectory /MIR /R:3 /W:5
 Remove-Item -Path $StagingDirectory\* -Recurse -Force
+
+if (Test-Path $TargetFSDirectory -PathType Container) {
+    $true
+} else {
+    throw "The target directory '$TargetFSDirectory' does not exist or is not a directory."
+}
+
+New-Item -Path $StagingFSDirectory -ItemType Directory -Force
+Remove-Item -Path $StagingFSDirectory\* -Recurse -Force
+Expand-Archive -Path $BackupFileFS -DestinationPath $StagingFSDirectory -Force
+robocopy $StagingFSDirectory $TargetFSDirectory /MIR /R:3 /W:5
+Remove-Item -Path $StagingFSDirectory\* -Recurse -Force
