416eefe70e6f17dc7c2a6c0bc6f43e8a72b75b9b
Jason Gan <jason.gan@pointwest.com>
2026-05-22 15:01:08 +0800

    Change backup drive

--- database-server-scripts/schedule-database-backup.sql
+++ database-server-scripts/schedule-database-backup.sql
@@ -3,14 +3,14 @@ GO
 
 EXEC dbo.create_daily_backup_job 
 	@job_name = 'Backup Day',
-	@backup_dir = 'H:\Backup\Day',
+	@backup_dir = 'G:\Backup\Day',
 	@time = 123000, -- 12:30 PM,
 	@retention = 2, -- 2 days
 	@sunday = 0; -- exclude Sunday
 
 EXEC dbo.create_daily_backup_job 
 	@job_name = 'Backup Night',
-	@backup_dir = 'H:\Backup\Night',
+	@backup_dir = 'G:\Backup\Night',
 	@time = 193000, -- 7:30 PM
 	@retention = 15, -- 15 days
 	@sunday = 0; -- exclude Sunday
